648e041835fa965d548c3f37b4cf759dba196324
mpdacey <80190692+mpdacey@users.noreply.github.com>
2025-05-28 13:18:56 +1000

    Average color intensity can now be applied.

--- Color Bit Crusher.lua
+++ Color Bit Crusher.lua
@@ -12,36 +12,39 @@ end
 
 -- Crushes a color channel to the nearest specified bit
 -- Parameters:
--- - colorValue:  The value of a pixel's color channel
--- - bitValue:    How many bits should be used when determining the crushed color
+-- - colorValue:       The value of a pixel's color channel
+-- - bitValue:         How many bits should be used when determining the crushed color
+-- - applyIntensity:   Apply the average intensity of a color using vacant bits.
 -- Returns the crushed color value.
-function CrushChannel(colorValue, bitValue)
+function CrushChannel(colorValue, bitValue, applyIntensity)
 	if bitValue == 0 then
 		return 0
 	end
 
 	local purgeBits = 8 - bitValue
 	
-	-- Shift bits to the right
-	colorValue = colorValue >> purgeBits
+	-- Purge bits out of bitValue's range
+	local purgedColor = colorValue >> purgeBits << purgeBits
 	
-	-- Shift the bits back if they don't match white
-	if colorValue == BYTE_DECIMAL >> purgeBits then
-		colorValue = BYTE_DECIMAL
-	else
-		colorValue = colorValue << purgeBits
+	-- If intensity is being preserved, re-add purged bits on a linear gradient
+	if applyIntensity then
+		local intensity = (purgedColor >> purgeBits) / (BYTE_DECIMAL >> purgeBits)
+		local gradient = BYTE_DECIMAL % (2^purgeBits) * intensity
+		gradient = math.floor(gradient + 0.5)
+		purgedColor = purgedColor + gradient
 	end
 	
-	return colorValue
+	return purgedColor
 end
 
 -- Crushes and clamps a given pixel to the nearest specified bit.
 -- Parameters:
--- - pc:      The pixel color in app
--- - pixel:   Selected pixel value.
--- - bits:    Number of bits to constrain the colors to.
+-- - pc:             The pixel color in app.
+-- - pixel:          Selected pixel value.
+-- - bits:           Number of bits to constrain the colors to.
+-- - applyIntensity: Apply the average intensity of a color using vacant bits.
 -- Returns crushed color
-function CrushColor(pc, pixel, bits)
+function CrushColor(pc, pixel, bits, applyIntensity)
 	if pc.rgbaA(pixel) == 0 then
 		return pixel
 	end
@@ -52,12 +55,12 @@ function CrushColor(pc, pixel, bits)
 	local blue = pc.rgbaB(pixel)
 	local alpha = pc.rgbaA(pixel)
 	
-	red = CrushChannel(red, bits.r)
-	green = CrushChannel(green, bits.g)
-	blue = CrushChannel(blue, bits.b)
+	red = CrushChannel(red, bits.r, applyIntensity)
+	green = CrushChannel(green, bits.g, applyIntensity)
+	blue = CrushChannel(blue, bits.b, applyIntensity)
 	
 	if bits.a >= 0 then
-		alpha = CrushChannel(alpha, bits.a)
+		alpha = CrushChannel(alpha, bits.a, applyIntensity)
 	end
 	
 	local newPixel = pc.rgba(red, green, blue, alpha)
@@ -66,16 +69,17 @@ end
 
 -- Crushes and clamps each pixel to the nearest specified bit.
 -- Parameters:
--- - base:    Base image used as reference
--- - pc:      The pixel color in app
--- - bits:    Specified bits that an image should be crushed to.
-function CrushImage(base, pc, bits)
+-- - base:           Base image used as reference.
+-- - pc:             The pixel color in app.
+-- - bits:           Specified bits that an image should be crushed to.
+-- - applyIntensity: Apply the average intensity of a color using vacant bits.
+function CrushImage(base, pc, bits, applyIntensity)
     local crunched = base:clone()
 	
     -- Iterates through each pixel in base
     for y = 0, base.height, 1 do
         for x = 0, base.width, 1 do
-			crunched:drawPixel(x, y, CrushColor(pc, base:getPixel(x,y), bits))
+			crunched:drawPixel(x, y, CrushColor(pc, base:getPixel(x,y), bits, applyIntensity))
         end
     end
 
@@ -121,6 +125,8 @@ local dlg = Dialog("Color Bit Crusher")
 		end
 	}
     dlg:newrow()
+	dlg:check{ id="apply", label="Apply intensity:", selected=true }
+	dlg:newrow()
     dlg:button{ id="confirm", text="Confirm" }
     dlg:button{ id="cancel", text="Cancel" }
     dlg:show()
@@ -150,7 +156,7 @@ if data.confirm then
 	end
 	
 	-- Allows undo functionality
-    cel.image = CrushImage(base, pc, bits)
+    cel.image = CrushImage(base, pc, bits, data.apply)
 
     -- Redraw pixels in app
     app.refresh()
